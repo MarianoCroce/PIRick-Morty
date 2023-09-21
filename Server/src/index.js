a15e17bb8e0d47c896a56cdf809dde3b67b6b893
MarianoCroce <marianoaguirre14@gmail.com>
2023-09-21 09:14:04 -0300

    Nuevos cambios, agregada DB

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,10 @@
+const { Sequelize } = require("sequelize");
 const app = require("./app")
 const PORT = 3001;
+const { conn } = require('./DB_connection')
 
 app.listen(PORT, () => {
+  conn.sync({force: true});
   console.log(`Server raised on port: ${PORT}`);
 });
 
